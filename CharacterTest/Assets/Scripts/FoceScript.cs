7451641d6977651ba3efc5244646cfcacce6d0dd
Seth Stoltenow <seth.stoltenow@gmail.com>
2018-02-11 14:39:57 -0600

    Initial commit
    Player character moving, jumping, and pointing weapon toward mouse cursor.

---
+++ CharacterTest/Assets/Scripts/FoceScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoceScript : MonoBehaviour {
+
+	//private bool isDead = false;
+	private Rigidbody2D spaceMan;
+
+	public float moveSpeed = 5f;
+	private float jumpSpeed = 350f;
+	//private float jumpDelay = .5f;
+	private float canJump = 0f;
+
+	// Use this for initialization
+	void Start () {
+		spaceMan = GetComponent<Rigidbody2D> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.D))
+			transform.Translate (new Vector2 (1, 0) * moveSpeed * Time.deltaTime);
+
+		if (Input.GetKey (KeyCode.A))
+			transform.Translate (new Vector2 (-1, 0) * moveSpeed * Time.deltaTime);
+
+		/*if (Input.GetKey (KeyCode.W)) 
+		{
+			spaceMan.AddForce (new Vector2(0, jumpSpeed));
+		}*/
+		
+	}
+
+	void OnCollisionStay2D(Collision2D coll ) // C#, type first, name in second
+	{
+		if (coll.gameObject.tag == "World" && (Input.GetKey (KeyCode.W)) && Time.time > canJump)
+		{
+			//Jump Script
+			spaceMan.AddForce (new Vector2(0, jumpSpeed));
+			canJump = Time.time + .75f;
+
+		}
+
+
+
+
+
+	}
+}
---
+++ CharacterTest/Assets/Scripts/gunRotation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunRotation : MonoBehaviour {
+	
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*float vertical = Input.GetAxis ("Vertical");
+		float horizontal = Input.GetAxis ("Horizontal");
+		transform.Rotate (0 , 0, horizontal);
+		*/
+
+		//Get the Screen positions of the object
+		Vector2 positionOnScreen = Camera.main.WorldToViewportPoint (transform.position);
+
+		//Get the Screen position of the mouse
+		Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+		//Get the angle between the points
+		float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+
+		//Ta Daaa
+		transform.rotation =  Quaternion.Euler (new Vector3(0f,0f,angle - 180));
+		
+	}
+
+	float AngleBetweenTwoPoints(Vector3 a, Vector3 b) {
+		return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+	}
+}
