1910c676c0558e1726fb943dc3754c8586e338a7
Fuad Mohamoud <fuad.mohamoud@und.edu>
2018-02-25 16:31:25 -0600

    More ALiens
    More more more Aliens!

--- CharacterTest/Assets/Scripts/FoceScript.cs
+++ CharacterTest/Assets/Scripts/FoceScript.cs
@@ -43,9 +43,5 @@ public class FoceScript : MonoBehaviour {
 
 		}
 
-
-
-
-
 	}
 }
--- CharacterTest/Assets/Scripts/Follow.cs
+++ CharacterTest/Assets/Scripts/Follow.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class Follow : MonoBehaviour {
 	private Rigidbody2D alien;
 	private Rigidbody2D playerRigid;
-	public float moveSpeed = 1f;
+	public float moveSpeed = 15f;
 	private GameObject player;
 	public float num;
 	private float jumpSpeed = 350f;
 	private float canJump = 0f;
+	private float canLeap= 0f;
+	private int counter=0;
+
+
+
 	// Use this for initialization
 	void Start () {
 		alien = GetComponent<Rigidbody2D> ();
@@ -17,25 +22,46 @@ public class Follow : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		 num= Random.Range(0,100);
-		 player= GameObject.FindWithTag("Player");
-		 float step = moveSpeed * Time.deltaTime;
-		 if(player){
-			playerRigid= player.GetComponent<Rigidbody2D> ();
-			if(num %7 ==0){	
-		      transform.position = Vector2.MoveTowards(alien.position, playerRigid.position, step);
-			}else if(num %10 ==0){
-			  transform.position = Vector2.MoveTowards(alien.position, playerRigid.position, -step);
-			}else if(num % 3 ==0){
+		 num= Random.Range(0,4);
+	}
+
+
+	void OnCollisionStay2D(Collision2D col){
+		if (col.gameObject.tag == "World" && Time.time > canJump){
+			player= GameObject.FindWithTag("Player");
+			float step = moveSpeed * Time.deltaTime;
+			if(player){
+				playerRigid= player.GetComponent<Rigidbody2D> ();
+				moveAlien(playerRigid,step,num);
+			}
+		}
+	  }
+
+	void moveAlien(Rigidbody2D thePlayer, float step, float randomNumber){
+		 if(randomNumber==0){
+			 //move closer
+		   transform.position = Vector2.MoveTowards(alien.position, playerRigid.position, step);
+		 }else if(randomNumber==1){
+			 //move away
+		    transform.position = Vector2.MoveTowards(alien.position, playerRigid.position, -step);
+		 }else if(randomNumber==2){
+		    //move closer and jump
+			transform.position = Vector2.MoveTowards(alien.position, playerRigid.position, step);
 				alien.AddForce (new Vector2(0, jumpSpeed));
 				if(Time.time > canJump){
 					canJump = Time.time + .75f;
 				}
+			
 			}else{
-
+			 //move away and jump
+            	transform.position = Vector2.MoveTowards(alien.position, playerRigid.position, -step);
+		
+				alien.AddForce (new Vector2(0, jumpSpeed));
+				if(Time.time > canJump){
+					canJump = Time.time + .75f;
+				}
+			
 			}
-		 }
-
 	}
 
 
