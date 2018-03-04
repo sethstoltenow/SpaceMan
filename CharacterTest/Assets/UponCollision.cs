019ebc9480ab6caff4cdfec967c6d713623612d6
Seth Stoltenow <seth.stoltenow@gmail.com>
2018-03-03 18:18:46 -0600

    Projectile Collision
    Collision enabled for projectiles. Next step: make AI lose health on collision

---
+++ CharacterTest/Assets/UponCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UponCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnCollisionEnter2D(Collision2D coll) {
+		//Instantiate an explosion object at location
+		//if (coll.gameObject.tag == "Enemy")
+			Destroy (gameObject);
+
+	}
+}
